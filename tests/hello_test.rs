3a5c3025e689e43024bf39f5982fa5e238407cbf
h0m1 <h0m1@proton.me>
2024-06-30 13:25:57 +0200

    added failing test

--- tests/hello_test.rs
+++ tests/hello_test.rs
@@ -17,3 +17,9 @@ fn check_value3() {
     let val = hello::make_string("hello", "hello");
     assert_eq!(val, String::from("hello hello"));
 }
+
+#[test]
+fn check_value4() {
+    let val = hello::make_string("hello", "hello");
+    assert_eq!(val, String::from("hello1 hello2"));
+}
